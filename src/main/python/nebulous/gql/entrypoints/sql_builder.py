96bf49f46703a8419d72f604bd036c7871a1d726
Oliver Rice <github@oliverrice.com>
2020-04-02 04:35:21 -0500

    refactor sql_builder

--- src/main/python/nebulous/gql/entrypoints/sql_builder.py
+++ src/main/python/nebulous/gql/entrypoints/sql_builder.py
@@ -102,85 +102,41 @@ def sql_builder(tree, parent_name=None):
     sqla_model = return_type.sqla_model
 
     if isinstance(return_type, TableType):
-        block_name = random_string()
-        table_name = sqla_model.table_name
-        if parent_name is None:
-            # If there is no parent, nodeId is mandatory
-            _, pkey_eq = tree["args"]["nodeId"]
-            pkey_clause = to_pkey_clause(tree, pkey_eq)
-            join_clause = ["true"]
-        else:
-            # If there is a parent no arguments are accepted
-            join_clause = to_join_clause(tree, parent_name)
-            pkey_clause = ["true"]
-
-        select_clause = []
-        for field in tree["fields"]:
-            if isinstance(field["return_type"], ScalarType):
-                select_clause.append(build_scalar(field, sqla_model))
-            else:
-                select_clause.append(build_relationship(field, block_name))
-
-        return row_block(
-            block_name=block_name,
-            table_name=table_name,
-            pkey_clause=pkey_clause,
-            join_clause=join_clause,
-            select_clause=select_clause,
-        )
+        return row_block(field=tree, parent_name=parent_name)
 
     if isinstance(return_type, ConnectionType):
-        block_name = random_string()
-        table_name = sqla_model.table_name
-        if parent_name is None:
-            join_conditions = ["true"]
-        else:
-            join_conditions = to_join_clause(tree, parent_name)
-
-        filter_conditions = to_conditions_clause(tree)
-        limit_clause = to_limit_clause(tree)
-        after_clause = to_after_clause(tree)
-
-        nodes_selects = []
-        edge_node_selects = []
-        for field in tree["fields"]:
-            if field["name"] == "nodes":
-                subfields = field["fields"]
-            elif field["name"] == "edges":
-                subfields = [x for x in field["fields"] if x["name"] == "node"][0]["fields"]
-
-            for subfield in subfields:
-                if isinstance(subfield["return_type"], ScalarType):
-                    elem = build_scalar(subfield, sqla_model)
-                else:
-                    elem = build_relationship(subfield, block_name)
-                if field["name"] == "nodes":
-                    nodes_selects.append(elem)
-                elif field["name"] == "edges":
-                    edge_node_selects.append(elem)
-
-        return connection_block(
-            block_name=block_name,
-            table_name=table_name,
-            join_conditions=join_conditions,
-            filter_conditions=filter_conditions,
-            nodes_selects=nodes_selects,
-            edge_node_selects=edge_node_selects,
-            limit=limit_clause,
-            after=after_clause,
-        )
+        return connection_block(field=tree, parent_name=parent_name)
 
 
 def sql_finalize(return_name, expr):
-    return f"""
-select
+    return f"""select
     jsonb_build_object('{return_name}', ({expr}))
     """
 
 
-def row_block(
-    block_name: str, table_name, pkey_clause, join_clause, select_clause: typing.List[str]
-):
+def row_block(field, parent_name=None):
+    return_type = field["return_type"]
+    sqla_model = return_type.sqla_model
+
+    block_name = random_string()
+    table_name = sqla_model.table_name
+    if parent_name is None:
+        # If there is no parent, nodeId is mandatory
+        _, pkey_eq = field["args"]["nodeId"]
+        pkey_clause = to_pkey_clause(field, pkey_eq)
+        join_clause = ["true"]
+    else:
+        # If there is a parent no arguments are accepted
+        join_clause = to_join_clause(field, parent_name)
+        pkey_clause = ["true"]
+
+    select_clause = []
+    for field in field["fields"]:
+        if isinstance(field["return_type"], ScalarType):
+            select_clause.append(build_scalar(field, sqla_model))
+        else:
+            select_clause.append(build_relationship(field, block_name))
+
     block = f"""
 (
     with {block_name} as (
@@ -202,16 +158,41 @@ def row_block(
     return block
 
 
-def connection_block(
-    block_name: str,
-    table_name: str,
-    join_conditions: typing.List[str],
-    filter_conditions: typing.List[str],
-    nodes_selects: typing.List[typing.Tuple[str, "expr"]],
-    edge_node_selects: typing.List[typing.Tuple[str, "expr"]],
-    limit: int = 10,
-    after: typing.Optional[str] = None,
-):
+def connection_block(field, parent_name):
+    return_type = field["return_type"]
+    sqla_model = return_type.sqla_model
+
+    block_name = random_string()
+    table_name = sqla_model.table_name
+    if parent_name is None:
+        join_conditions = ["true"]
+    else:
+        join_conditions = to_join_clause(field, parent_name)
+
+    filter_conditions = to_conditions_clause(field)
+    limit = to_limit_clause(field)
+    after = to_after_clause(field)
+
+    nodes_selects = []
+    edge_node_selects = []
+    for cfield in field["fields"]:
+        if cfield["name"] == "nodes":
+            subfields = cfield["fields"]
+        elif cfield["name"] == "edges":
+            subfields = [x for x in cfield["fields"] if x["name"] == "node"][0]["fields"]
+
+        for subfield in subfields:
+            if isinstance(subfield["return_type"], ScalarType):
+                elem = build_scalar(subfield, sqla_model)
+            else:
+                elem = build_relationship(subfield, block_name)
+            if cfield["name"] == "nodes":
+                nodes_selects.append(elem)
+            elif cfield["name"] == "edges":
+                edge_node_selects.append(elem)
+
+    # check if cursor is required
+
     block = f"""
 (
     with {block_name} as (
@@ -220,7 +201,7 @@ def connection_block(
         where 
             ({"and".join(join_conditions) or 'true'})
             and ({"and".join(filter_conditions) or 'true'})
- import            and ({after or 'true'})
+            and ({after or 'true'})
         limit
             {min(limit, 10)}
     )
