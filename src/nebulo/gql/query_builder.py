7e84076e443d978ca67711053fe5e42cc9790cc9
Oliver Rice <github@oliverrice.com>
2020-05-26 06:49:06 -0500

    arbitrarily nested to-many (no to-one support))

--- src/nebulo/gql/query_builder.py
+++ src/nebulo/gql/query_builder.py
@@ -6,26 +6,31 @@ import string
 import typing
 from functools import lru_cache
 
-from cachetools import cached
 from nebulo.config import Config
 from nebulo.gql.alias import CompositeType, ConnectionType, ScalarType, TableType
 from nebulo.gql.parse_info import ASTNode
 from nebulo.gql.relay.cursor import to_cursor_sql
 from nebulo.gql.relay.node_interface import NodeID, to_global_id_sql
-from nebulo.sql.inspect import (
-    get_columns,
-    get_primary_key_columns,
-    get_relationships,
-    get_table_name,
-)
+from nebulo.sql.inspect import get_columns, get_primary_key_columns, get_relationships, get_table_name
 from nebulo.sql.table_base import TableProtocol
-from sqlalchemy import Column
+from sqlalchemy import (
+    Column,
+    and_,
+    asc,
+    column,
+    create_engine,
+    desc,
+    func,
+    literal,
+    literal_column,
+    select,
+    table,
+    text,
+    tuple_,
+)
 from sqlalchemy.orm import RelationshipProperty
 from sqlalchemy.sql.compiler import StrSQLCompiler
-
-
-from sqlalchemy import text, column, select, literal_column, func, literal, and_, asc, desc, tuple_, table
-from sqlalchemy import create_engine
+from sqlalchemy.sql.selectable import Alias
 
 
 def sql_builder(tree: ASTNode, parent_name: typing.Optional[str] = None) -> str:
@@ -58,9 +63,7 @@ def field_name_to_column(sqla_model: TableProtocol, gql_field_name: str) -> Colu
 
 
 @lru_cache()
-def field_name_to_relationship(
-    sqla_model: TableProtocol, gql_field_name: str
-) -> RelationshipProperty:
+def field_name_to_relationship(sqla_model: TableProtocol, gql_field_name: str) -> RelationshipProperty:
     for relationship in get_relationships(sqla_model):
         if Config.relationship_name_mapper(relationship) == gql_field_name:
             return relationship
@@ -70,18 +73,14 @@ def field_name_to_relationship(
 def to_join_clause(field: ASTNode, parent_block_name: str) -> typing.List[str]:
     parent_field = field.parent
     assert parent_field is not None
-    relation_from_parent = field_name_to_relationship(
-        parent_field.return_type.sqla_model, field.name
-    )
+    relation_from_parent = field_name_to_relationship(parent_field.return_type.sqla_model, field.name)
     local_table_name = get_table_name(field.return_type.sqla_model)
 
     join_clause = []
     for parent_col, local_col in relation_from_parent.local_remote_pairs:
         parent_col_name = parent_col.name
         local_col_name = local_col.name
-        join_clause.append(
-            f"{parent_block_name}.{parent_col_name} = {local_table_name}.{local_col_name}"
-        )
+        join_clause.append(f"{parent_block_name}.{parent_col_name} = {local_table_name}.{local_col_name}")
     return join_clause
 
 
@@ -165,9 +164,7 @@ def to_conditions_clause(field: ASTNode) -> typing.List[str]:
     return res
 
 
-def build_scalar(
-    field: ASTNode, sqla_model: TableProtocol
-) -> typing.Tuple[str, typing.Union[str, StrSQLCompiler]]:
+def build_scalar(field: ASTNode, sqla_model: TableProtocol) -> typing.Tuple[str, typing.Union[str, StrSQLCompiler]]:
     return_type = field.return_type
     if return_type == NodeID:
         return (field.alias, to_global_id_sql(sqla_model))
@@ -181,8 +178,9 @@ def build_relationship(field: ASTNode, block_name: str) -> typing.Tuple[str, str
 
 
 def sql_finalize(return_name: str, expr: str) -> str:
-    final =  select([func.jsonb_build_object(literal(return_name), expr.c.ret_json).label('json')]).select_from(expr)
+    final = select([func.jsonb_build_object(literal(return_name), expr.c.ret_json).label("json")]).select_from(expr)
     return final
+
 
 def row_block(field: ASTNode, parent_name: typing.Optional[str] = None) -> str:
     return_type = field.return_type
@@ -227,18 +225,12 @@ def row_block(field: ASTNode, parent_name: typing.Optional[str] = None) -> str:
     return block
 
 
-@cached(cache={}, key=lambda x: x.return_type.sqla_model)
-def to_order_clause(field: ASTNode) -> str:
-    sqla_model = field.return_type.sqla_model
-    return "(" + ", ".join([x.name for x in get_primary_key_columns(sqla_model)]) + ")"
-
-
 def check_has_total(field: ASTNode) -> bool:
     "Check if 'totalCount' is requested in the query result set"
     return any(x.name in "totalCount" for x in field.fields)
 
 
-def connection_block(field: ASTNode, parent_name: typing.Optional[str]):
+def connection_block(field: ASTNode, parent_name: typing.Optional[str]) -> Alias:
     return_type = field.return_type
     sqla_model = return_type.sqla_model
 
@@ -265,15 +257,11 @@ def connection_block(field: ASTNode, parent_name: typing.Optional[str]):
     node_alias = field.get_subfield_alias(["edges", "node"])
     cursor_alias = field.get_subfield_alias(["edges", "cursor"])
 
-    pageInfo_alias = (field.get_subfield_alias(["pageInfo"]))
-    hasNextPage_alias = (field.get_subfield_alias(["pageInfo", "hasNextPage"]))
-    hasPreviousPage_alias = (
-        field.get_subfield_alias(["pageInfo", "hasPreviousPage"])
-    )
-    startCursor_alias = (field.get_subfield_alias(["pageInfo", "startCursor"]))
-    endCursor_alias = (field.get_subfield_alias(["pageInfo", "endCursor"]))
-
-
+    pageInfo_alias = field.get_subfield_alias(["pageInfo"])
+    hasNextPage_alias = field.get_subfield_alias(["pageInfo", "hasNextPage"])
+    hasPreviousPage_alias = field.get_subfield_alias(["pageInfo", "hasPreviousPage"])
+    startCursor_alias = field.get_subfield_alias(["pageInfo", "startCursor"])
+    endCursor_alias = field.get_subfield_alias(["pageInfo", "endCursor"])
 
     def build_scalar_select(field: ASTNode, sqla_model: TableProtocol):
         return_type = field.return_type
@@ -292,22 +280,17 @@ def connection_block(field: ASTNode, parent_name: typing.Optional[str]):
                 if edge_field.name == "node":
                     for subfield in edge_field.fields:
                         # Does anything other than NodeID go here?
-                        if isinstance(
-                            subfield.return_type, (ScalarType, CompositeType)
-                        ):
+                        if isinstance(subfield.return_type, (ScalarType, CompositeType)):
                             elem = build_scalar(subfield, sqla_model)
                             c = build_scalar_select(subfield, sqla_model)
                             new_edge_node_selects.append(c)
                         else:
                             elem = build_relationship(subfield, block_name)
                             new_relation_selects.append(elem)
-                            print('appended relation')
                         if cfield.name == "edges":
                             edge_node_selects.append(elem)
                         # Other than edges, pageInfo, and cursor stuff is
                         # all handled by default
-
-
 
     core_model = sqla_model.__table__
 
@@ -315,113 +298,92 @@ def connection_block(field: ASTNode, parent_name: typing.Optional[str]):
     reverse_order_clause = [desc(col) for col in get_primary_key_columns(sqla_model)]
 
     total_block = (
-        select([
-            func.count(1).label('total_count')
-        ])
+        select([func.count(1).label("total_count")])
         .select_from(core_model)
         .where(
             and_(
                 # Join clause
-                text("and".join(join_conditions) or 'true'),
+                text("and".join(join_conditions) or "true"),
                 # Conditions
-                text(("and".join(filter_conditions) or 'true')),
+                text(("and".join(filter_conditions) or "true")),
                 # Skip option
-                has_total
+                has_total,
             )
         )
-    ).alias(block_name + '_total')
+    ).alias(block_name + "_total")
 
     # Select the right stuff
     p1_block = (
-        select([
-            *new_edge_node_selects,
-            select([text(str(to_global_id_sql(sqla_model)))]).label('nodeId'),
-            func.row_number().over().label('_row_num'),
-        ])
+        select(
+            [
+                *new_edge_node_selects,
+                select([text(str(to_global_id_sql(sqla_model)))]).label("nodeId"),
+                func.row_number().over().label("_row_num"),
+            ]
+        )
         .select_from(core_model)
         .where(
             and_(
                 # Join clause
-                text("and".join(join_conditions) or 'true'),
+                text("and".join(join_conditions) or "true"),
                 # Conditions
-                text(("and".join(filter_conditions) or 'true')),
+                text(("and".join(filter_conditions) or "true")),
                 # Pagination
-                text(pagination)
+                text(pagination),
             )
         )
-        .order_by(
-            *(reverse_order_clause if is_page_before else order_clause),
-            *order_clause
-        )
-        .limit(
-            limit+1
-        )
-    ).alias(block_name + '_p1')
+        .order_by(*(reverse_order_clause if is_page_before else order_clause), *order_clause)
+        .limit(limit + 1)
+    ).alias(block_name + "_p1")
 
     # Drop maybe extra row
 
-    p2_block = (
-        select(p1_block.c)
-        .select_from(p1_block)
-        .limit(limit)
-    ).alias(block_name + '_p2')
+    p2_block = (select(p1_block.c).select_from(p1_block).limit(limit)).alias(block_name + "_p2")
 
-    ordering = desc(literal_column('_row_num')) if is_page_before else asc(literal_column('_row_num'))
+    ordering = desc(literal_column("_row_num")) if is_page_before else asc(literal_column("_row_num"))
 
-    p3_block = (
-         select([
-             *p2_block.c
-         ])
-        .select_from(p2_block)
-        .order_by(ordering)
-    ).alias(block_name)
+    p3_block = (select([*p2_block.c]).select_from(p2_block).order_by(ordering)).alias(block_name)
 
-    relations = []
+    relations = func.jsonb_build_object()
     for key, value in new_relation_selects:
-        relations.append(literal(key))
-        dial_eng = create_engine('postgresql://')
-        sql =  text(str(value.compile(compile_kwargs={'literal_binds': True, 'engine': dial_eng})))
-        relations.append(sql)
-        #select([value.c.ret_json]).select_from(value).alias())
+        relations = relations.op("||")(func.jsonb_build_object(literal(key), value.as_scalar()))
 
-    #final =  select([func.jsonb_build_object(literal(return_name), expr.c.ret_json).label('json')]).select_from(expr)
-
-
+    from sqlalchemy.dialects.postgresql import JSONB
 
     final = (
-        select([
-            func.jsonb_build_object(
-                literal(totalCount_alias), func.min(total_block.c.total_count),
-
-                literal(pageInfo_alias), func.jsonb_build_object(
-                    # literal(hasNextPage_alias), text(f"(select count(*) from {block_name}) < ")(select has_next from has_next_page),
-                    #literal(hasPreviousPage_alias), {'true' if is_page_after else 'false'},
-                    literal(startCursor_alias), func.array_agg(p3_block.c.nodeId)[1],
-                    literal(endCursor_alias), func.array_agg(p3_block.c.nodeId)[func.array_upper(func.array_agg(p3_block.c.nodeId), 1)],
-                ),
-
-                literal(edges_alias), func.jsonb_agg(
+        select(
+            [
+                func.jsonb_build_object(
+                    literal(totalCount_alias),
+                    func.min(total_block.c.total_count),
+                    literal(pageInfo_alias),
                     func.jsonb_build_object(
-                        literal(cursor_alias), p3_block.c.nodeId,
-                        literal(node_alias), func.row_to_json(literal_column(block_name)),
-                        *relations
-                    )
-                    
-
-                )
-            ).label('ret_json')
-        ])
+                        # literal(hasNextPage_alias), text(f"(select count(*) from {block_name}) < ")(select has_next from has_next_page),
+                        # literal(hasPreviousPage_alias), {'true' if is_page_after else 'false'},
+                        literal(startCursor_alias),
+                        func.array_agg(p3_block.c.nodeId)[1],
+                        literal(endCursor_alias),
+                        func.array_agg(p3_block.c.nodeId)[func.array_upper(func.array_agg(p3_block.c.nodeId), 1)],
+                    ),
+                    literal(edges_alias),
+                    func.jsonb_agg(
+                        func.jsonb_build_object(
+                            literal(cursor_alias),
+                            p3_block.c.nodeId,
+                            literal(node_alias),
+                            func.cast(func.row_to_json(literal_column(block_name)), JSONB()).op("||")(relations),
+                        )
+                    ),
+                ).label("ret_json")
+            ]
+        )
         .select_from(p3_block)
         .select_from(total_block)
     ).alias()
 
-    #dial_eng = create_engine('postgresql://')
-    #sql =  str(final.compile(compile_kwargs={'literal_binds': True, 'engine': dial_eng}))
-    #print(sql)
-
     return final
 
-    
+
 def secure_random_string(length: int = 8) -> str:
     letters = string.ascii_lowercase
     return "".join([secrets.choice(letters) for _ in range(length)])
