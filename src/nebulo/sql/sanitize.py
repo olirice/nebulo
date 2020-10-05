089a562bdd807a599d196390fa016f7afc3c1f6b
Oliver Rice <github@oliverrice.com>
2020-10-05 03:46:23 -0500

    remove sanitize helper

--- src/nebulo/sql/sanitize.py
+++ src/nebulo/sql/sanitize.py
@@ -5,8 +5,3 @@ import string
 def secure_random_string(length=8) -> str:
     letters = string.ascii_lowercase
     return "".join([secrets.choice(letters) for _ in range(length)])
-
-
-def sanitize(text: str) -> str:
-    escape_key = secure_random_string()
-    return f"${escape_key}${text}${escape_key}$"
