0c282cf6a10632da14fbb2133ad35e09a71d92cb
Oliver Rice <github@oliverrice.com>
2020-10-05 06:59:30 -0500

    bump version and release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="nebulo",
-    version="0.1.0",
+    version="0.1.1",
     description="Nebulo: Reflect RDBMS to GraphQL API",
     author="Oliver Rice",
     author_email="oliver@oliverrice.com",
